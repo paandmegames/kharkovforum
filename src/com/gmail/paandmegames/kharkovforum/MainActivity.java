2c901b80cba0b9b8902d78fc3fd4af4024678217
Rad <okilimnik@gmail.com>
2015-04-03 02:32:49 +0300

    added volley for http requests

--- src/com/gmail/paandmegames/kharkovforum/MainActivity.java
+++ src/com/gmail/paandmegames/kharkovforum/MainActivity.java
@@ -1,8 +1,28 @@
 package com.gmail.paandmegames.kharkovforum;
 
+import java.net.HttpURLConnection;
+
+import org.apache.http.client.HttpClient;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HttpClientStack;
+import com.android.volley.toolbox.HttpStack;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.net.Uri;
+import android.net.http.AndroidHttpClient;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -15,16 +35,70 @@ import android.widget.TextView;
 public class MainActivity extends ActionBarActivity {
 
 
+	private HttpStack stack;
+
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		//setContentView(R.layout.activity_main);
-		setContentView(R.layout.authorization_fragment);
+		setContentView(R.layout.activity_main);
+		final TextView mTextView = (TextView) findViewById(R.id.result_text);
+		
+		
+		
+	
+		// If the device is running a version >= Gingerbread...
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+		    // ...use HttpURLConnection for stack.
+			stack = new HurlStack();
+		} else {
+		    // ...use AndroidHttpClient for stack.
+			stack = new HttpClientStack(AndroidHttpClient.newInstance("User-Agent"));
+		}
+		Network network = new BasicNetwork(stack);
+		
+		RequestQueue mRequestQueue;
+
+		// Instantiate the cache
+		Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+
+
+		// Instantiate the RequestQueue with the cache and network.
+		mRequestQueue = new RequestQueue(cache, network);
+
+		// Start the queue
+		mRequestQueue.start();
+
+		String url ="http://www.google.com";
+		
+		// Formulate the request and handle the response.
+		StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+		        new Response.Listener<String>() {
+		    @Override
+		    public void onResponse(String response) {
+		    	 mTextView.setText("Response is: "+ response.substring(0,500));
+
+		    }
+		},
+		    new Response.ErrorListener() {
+		        @Override
+		        public void onErrorResponse(VolleyError error) {
+		        	mTextView.setText("That didn't work!");
+
+		    }
+		});
+
+		// Add the request to the RequestQueue.
+		mRequestQueue.add(stringRequest);
 
 
 	}
 	
 
+	
+	
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
